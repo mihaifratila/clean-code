ae7d238e4105ceac01d81c6b5a797b24f380a5a3
mihaifratila <mfratila@griddynamics.com>
2023-02-18 14:29:54 +0200

    Update the Test Class so that it covers the Backstage passes cases

--- src/test/java/com/a/introduction/gildedrose/GildedRoseARefactorTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseARefactorTest.java
@@ -9,6 +9,7 @@ public class GildedRoseARefactorTest {
 
 	public static final String DEFAULT_ITEM = "DEFAULT_ITEM";
 	private static final String AGED_BRIE = "Aged Brie";
+	private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
 	public static final int NOT_EXPIRED_SELLIN = 16;
 	public static final int EXPIRED_SELLIN = -2;
 	private static final int POSITIVE_SELLIN_LESS_THAN_5 = 2;
@@ -72,14 +73,14 @@ public class GildedRoseARefactorTest {
 	public void unexpiredAgedBrie_QualityDoesNotGoBeyondMaximumValue() {
 		// setup
 		GildedRose app = createGildedRoseWithOneItem(AGED_BRIE,
-				POSITIVE_SELLIN_LESS_THAN_5, MAXIMUM_QUALITY);
+				NOT_EXPIRED_SELLIN, MAXIMUM_QUALITY);
 
 		// invoke
 		app.updateQuality();
 
 		// verify
 		Item expected = new Item(AGED_BRIE,
-				POSITIVE_SELLIN_LESS_THAN_5 - 1, MAXIMUM_QUALITY);
+				NOT_EXPIRED_SELLIN - 1, MAXIMUM_QUALITY);
 
 		// expected, actual
 		AssertItem(expected, app.items[0]);
@@ -97,6 +98,56 @@ public class GildedRoseARefactorTest {
 		// verify
 		Item expected = new Item(AGED_BRIE,
 				EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 2);
+
+		// expected, actual
+		AssertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void backstagePassBeyond10Days_QualityIncreasedBy1() {
+		// setup
+		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES,
+				NOT_EXPIRED_SELLIN, DEFAULT_QUALITY);
+
+		// invoke
+		app.updateQuality();
+
+		// verify
+		Item expected = new Item(BACKSTAGE_PASSES,
+				NOT_EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 1);
+
+		// expected, actual
+		AssertItem(expected, app.items[0]);
+	}
+	@Test
+	public void backstagePassBetween5And10Days_QualityIncreasedBy2() {
+		// setup
+		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES,
+				SELLIN_BETWEEN_5_AND_10, DEFAULT_QUALITY);
+
+		// invoke
+		app.updateQuality();
+
+		// verify
+		Item expected = new Item(BACKSTAGE_PASSES,
+				SELLIN_BETWEEN_5_AND_10 - 1, DEFAULT_QUALITY + 2);
+
+		// expected, actual
+		AssertItem(expected, app.items[0]);
+	}
+
+	@Test
+	public void backstagePassLessThan5Days_QualityIncreasedBy2() {
+		// setup
+		GildedRose app = createGildedRoseWithOneItem(BACKSTAGE_PASSES,
+				POSITIVE_SELLIN_LESS_THAN_5, DEFAULT_QUALITY);
+
+		// invoke
+		app.updateQuality();
+
+		// verify
+		Item expected = new Item(BACKSTAGE_PASSES,
+				POSITIVE_SELLIN_LESS_THAN_5 - 1, DEFAULT_QUALITY + 3);
 
 		// expected, actual
 		AssertItem(expected, app.items[0]);
