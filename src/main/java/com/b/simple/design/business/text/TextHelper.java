86d63077359a8014b94e535840e1539b57721174
mihaifratila <mfratila@griddynamics.com>
2023-02-18 17:40:47 +0200

    Update the TextHelper Class with the correct implementations and to reflect Clean Code principles

--- src/main/java/com/b/simple/design/business/text/TextHelper.java
+++ src/main/java/com/b/simple/design/business/text/TextHelper.java
@@ -3,10 +3,25 @@ package com.b.simple.design.business.text;
 public class TextHelper {
 
 	public String swapLastTwoCharacters(String str) {
-		return null;
+		int length = str.length();
+
+		if (length < 2)
+			return str;
+
+		char lastChar = str.charAt(length - 1);
+		char secondLastChar = str.charAt(length - 2);
+		return str.substring(0, length - 2) + lastChar + secondLastChar;
 	}
 
+
 	public String truncateAInFirst2Positions(String str) {
-		return null;
+		if (str.length() < 2)
+			return str.replaceAll("A", "");
+
+		String firstTwoChars = str.substring(0, 2).replaceAll("A", "");
+		firstTwoChars = firstTwoChars.replaceAll("A", "");
+		String restOfString = str.substring(2);
+
+		return firstTwoChars + restOfString;
 	}
 }
